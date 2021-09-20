f1ed0ea62d3d69c2a9cfa62148b1e0911e48d1e5
Luis Torres <luiseduardotorresfigueroa@gmail.com>
2021-09-19 19:44:51 -0500

    U

--- src/app/pages/reservas/reservas.component.ts
+++ src/app/pages/reservas/reservas.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-reservas',
-  templateUrl: './reservas.component.html',
-  styleUrls: ['./reservas.component.scss']
+	selector: 'app-reservas',
+	templateUrl: './reservas.component.html',
+	styleUrls: ['./reservas.component.scss'],
 })
 export class ReservasComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
+	ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+	alerta() {
+		console.log("se clikeo!!");
+		let btn = document.getElementsByClassName('btn-red')[0] as HTMLElement;
+		btn.style.background= "blue";
+	}
 }
