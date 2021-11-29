632352629232147da276f4f026c930a2ef9f60a4
ltorres98 <luiseduardotorresfigueroa@gmail.com>
2021-11-29 15:28:04 -0500

    U

--- src/app/pages/agregar-citas/agregar-citas.component.ts
+++ src/app/pages/agregar-citas/agregar-citas.component.ts
@@ -15,20 +15,13 @@ export class AgregarCitasComponent implements OnInit {
     sedes: any[] = [];
     horas: any[] = [];
     date = new Date();
-<<<<<<< HEAD
     paciente: any = '';
-=======
->>>>>>> 6c9c74438ea5afe8b20150b09e5b554f8c5f29e1
 
     formCitasUser = new FormGroup({
         comentario: new FormControl('', Validators.required),
         fecha_cita: new FormControl('', Validators.required),
         sede: new FormControl('', Validators.required),
-<<<<<<< HEAD
         // paciente: new FormControl('', Validators.required),
-=======
-        paciente: new FormControl('', Validators.required),
->>>>>>> 6c9c74438ea5afe8b20150b09e5b554f8c5f29e1
         area: new FormControl('', Validators.required),
         hora: new FormControl('', Validators.required)
     });
@@ -37,7 +30,6 @@ export class AgregarCitasComponent implements OnInit {
 
         private _toastr: ToastrService
     ) {
-<<<<<<< HEAD
         let user: any = localStorage.getItem('userCurrent');
         const { id_usuario } = JSON.parse(user);
 
@@ -45,8 +37,6 @@ export class AgregarCitasComponent implements OnInit {
             this.paciente = resp[0].id_paciente;
             console.log('ID PACIENTE', this.paciente);
         });
-=======
->>>>>>> 6c9c74438ea5afe8b20150b09e5b554f8c5f29e1
         this._citasSrv.getAreas().subscribe(resp => {
             this.areas = resp;
         });
@@ -69,11 +59,7 @@ export class AgregarCitasComponent implements OnInit {
     saveCita() {
         if (this.formCitasUser.valid) {
             this._citasSrv
-<<<<<<< HEAD
                 .postCita(this.formCitasUser.value, this.date, this.paciente)
-=======
-                .postCita(this.formCitasUser.value, this.date)
->>>>>>> 6c9c74438ea5afe8b20150b09e5b554f8c5f29e1
                 .subscribe(resp => {
                     console.log(resp);
                     this._toastr.success(
